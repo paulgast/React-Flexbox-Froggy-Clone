63681345f5ad78294b32d14c8a3f5d05ccc36bff
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-11 14:35:09 +0100

    Creation de la mare (Pond) et du texte des consignes (instructions)

---
+++ src/Components/Instructions.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Instructions(props) {
+    return(
+        <p>{props.text}</p>
+    )
+}
+
+export default Instructions
---
+++ src/Components/Pond.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Pond() {
+    return(
+        <div className="Pound">
+            <h3>This is the pond</h3>
+        </div>
+    )
+}
+
+export default Pond
