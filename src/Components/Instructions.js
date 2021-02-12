088bb8080f4de286578655d33c37236cf91e5ca3
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-12 09:38:17 +0100

    Ajout des props.children au composant Text

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -1,21 +1,25 @@
 import React from "react"
 import Text from "./Text"
 
-function Instructions(props) {
+function Instructions() {
     return(
-        <div>
-            <p>
-            <p>Par exemple, justify-content: flex-end; bougera la grenouille vers la droite.</p>
-                <ul>
-                    <li>flex-start : Les éléments s'alignent au côté gauche du conteneur.</li>
-                    <li>flex-end : Les éléments s'alignent au côté droit du conteneur.</li>
-                    <li>center : Les éléments s'alignent au centre du conteneur.</li>
-                    <li>space-between : Les éléments s'affichent avec un espace égal entre eux.</li>
-                    <li>space-around : Les éléments s'affichent avec un espacement égal à l'entour d'eux.</li>
-                </ul>
-            </p>
-        </div>
+        <Text>
+            <div className="text">
+                <p>
+                    {"Bienvenue à Flexbox Froggy, un jeu où vous aidez Froggy la grenouille et ses amis en écrivant du code CSS! Guidez cette grenouille au nénuphar à la droite de l'étang en utilisant la propriété justify-content, qui aligne les éléments horizontalement et accepte les valeurs suivantes:"}
+                    <ul>
+                        <li>{"flex-start : Les éléments s'alignent au côté gauche du conteneur."}</li>
+                        <li>{"flex-end : Les éléments s'alignent au côté droit du conteneur."}</li>
+                        <li>{"center : Les éléments s'alignent au centre du conteneur."}</li>
+                        <li>{"space-between : Les éléments s'affichent avec un espace égal entre eux."}</li>
+                        <li>{"space-around : Les éléments s'affichent avec un espacement égal à l'entour d'eux."}</li>
+                    </ul>
+                    <p>{"Par exemple, justify-content: flex-end; bougera la grenouille vers la droite"}</p>
+                </p>
+            </div>
+        </Text>
     )
 }
 
 export default Instructions
+
--- src/Components/Text.js
+++ src/Components/Text.js
@@ -1,17 +1,9 @@
 import React from "react"
 
-function Text() {
+function Text(props) {
     return(
         <div>
-            <p>
-                <ul>
-                    <li>flex-start : Les éléments s'alignent au côté gauche du conteneur.</li>
-                    <li>flex-end : Les éléments s'alignent au côté droit du conteneur.</li>
-                    <li>center : Les éléments s'alignent au centre du conteneur.</li>
-                    <li>space-between : Les éléments s'affichent avec un espace égal entre eux.</li>
-                    <li>space-around : Les éléments s'affichent avec un espacement égal à l'entour d'eux.</li>
-                </ul>
-            </p>
+            <p>{props.children}</p>
         </div>
     )
 }
