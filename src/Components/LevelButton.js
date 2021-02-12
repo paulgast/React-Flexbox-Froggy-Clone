1ae194494d4a3be26da1b6436c5903f4dbff6c7f
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-12 15:55:45 +0100

    Implémentation de setState sur les components Buttons et des méthodes pour ajouter ou soustraire un niveau

--- src/Components/LevelButton.js
+++ src/Components/LevelButton.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react"
 
-function LevelButton(props){
+function LevelButton(){
 
     const [level, setLevel] = useState(1)
 
+    function decrement() {
+        setLevel(prevLevel => prevLevel - 1)
+    }
+
+    function increment() {
+        setLevel(prevLevel => prevLevel + 1)
+    }
         return(
         <div>
-            <button>Précédent</button>
-            <button>Niveau {level} de 24</button>
-            <button>Suivant</button>
+            <button className="levelButton"onClick = { decrement } >◢</button>
+            <button className="levelButton">Niveau {level} de 24 ▼</button>
+            <button className="levelButton"onClick = { increment }>◣</button>
         </div>
         )
     }
