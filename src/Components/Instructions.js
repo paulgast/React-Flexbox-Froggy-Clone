e47ce4a4914f1595273efe7934a3fcaa70a17426
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-14 09:58:44 +0100

    Modification des instructions de display dans le component Text

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -2,11 +2,11 @@ import React from "react"
 import Text from "./Text"
 import levelsData from "./levelsData"
 
-
 function Instructions() {
     return(
         <Text>
             {levelsData[0].instructions}
+            
         </Text>
     )
 }
--- src/Components/levelsData.js
+++ src/Components/levelsData.js
@@ -1,11 +1,15 @@
-import React from "react"
-
 const levelsData = [
 
     {
         id: 1,
         instructions: "Bienvenue à Flexbox Froggy, un jeu où vous aidez Froggy la grenouille et ses amis en écrivant du code CSS! Guidez cette grenouille au nénuphar à la droite de l'étang en utilisant la propriété justify-content, qui aligne les éléments horizontalement et accepte les valeurs suivantes",
-        properties: [{ "text": "flex-start : Les éléments s'alignent au côté gauche du conteneur.", "children": []}, { "text": "flex-end : Les éléments s'alignent au côté droit du conteneur.", "children": []}, { "text": "center : Les éléments s'alignent au centre du conteneur.", "children": []}, { "text": "space-between : Les éléments s'affichent avec un espace égal entre eux.", "children": []}, { "text": "space-around : Les éléments s'affichent avec un espacement égal à l'entour d'eux.", "children": []}]
+        properties: [
+            {text: "flex-start : Les éléments s'alignent au côté gauche du conteneur."},
+            {text: "flex-end : Les éléments s'alignent au côté droit du conteneur."}, 
+            {text: "center : Les éléments s'alignent au centre du conteneur."}, 
+            {text: "space-between : Les éléments s'affichent avec un espace égal entre eux."},
+            {text: "space-around : Les éléments s'affichent avec un espacement égal à l'entour d'eux."}
+        ]
     },
     {
         id: 2,
