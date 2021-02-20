6dad95a0ca656424e21e2937813a4bd70f4dd7bf
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-20 13:05:52 +0100

    Implémentation des méthodes décrementLevel et incrementLevel sur les buttonsLevels

--- src/Components/LevelButton.js
+++ src/Components/LevelButton.js
@@ -8,11 +8,12 @@ function LevelButton(props) {
                 <div>
                     {context.level <= 1 ? 
                         <button className="levelButton" disabled style={{backgroundColor: 'rgba(255,255,255,0.5)'}}>◢</button> :
-                        <button className="levelButton" onClick = {context.decrementLevel}>◢</button>} 
+                        <button className="levelButton" onClick = {context.decrementLevel}>◢</button>
+                        } 
 
-                        <button className="levelButton" >Niveau {context.level} de 24 ▼</button>
+                    <button className="levelButton" >Niveau {context.level} de 24 ▼</button>
 
-                        {context.level  >= 24 ?
+                    {context.level  >= 24 ?
                         <button className="levelButton" disabled style={{backgroundColor: 'rgba(255,255,255,0.5)'}}>◣</button> :
                         <button className="levelButton" onClick = {context.incrementLevel}>◣</button>
                     }
